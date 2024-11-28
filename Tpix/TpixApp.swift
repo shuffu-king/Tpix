a9660e3eaf084797bcbb70f0a0466a2f3eeff97f
shuffu-king <shafau_ayo@outlook.com>
2024-11-28 18:00:03 -0500

    Initial Commit

---
+++ Tpix/TpixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TpixApp.swift
+//  Tpix
+//
+//  Created by Ayo Shafau on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TpixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
